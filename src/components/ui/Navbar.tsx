40bf5319b5fcf20ed5d92e87fd81fd48ce8cc557
Benjoe Oates Carandang <benjoe.carandang@gmail.com>
2025-06-01 22:40:49 +0800

    navbar responsiveness

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -7,78 +7,76 @@ const Navbar = () => {
   const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
-    <nav className={`bg-white border-gray-1 ${
-          menuOpen ? "pb-0 pt-2" : "py-2"
-        } border-b-[1px] z-10`}>
-      <div
-        className={`max-w-screen-xl flex flex-wrap items-center justify-between mx-auto ${
-          menuOpen ? "pt-4" : "py-4"
-        }`}
-      >
-        <Link
-          to="/pokemons"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-gray-9">
-            Benjoemon
-          </span>
-        </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-          onClick={toggleMenu}
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
+    <>
+      <nav className="bg-white border-gray-1 border-b-[1px] dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-6">
+          <a
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
             />
-          </svg>
-        </button>
-        <div
-          className={`${menuOpen ? "" : "hidden"} w-full md:block md:w-auto`}
-          id="navbar-default"
-        >
-          <ul className="font-bold flex flex-col p-4 md:p-0 mt-[1.5rem] border md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
-            <li>
-              <Link
-                className="block py-2 px-3 text-gray-9 rounded m-0"
-                aria-current="page"
-                to="/pokemons"
-              >
-                Pokémons
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="block py-2 px-3 text-gray-9 rounded m-0"
-                to="/pokemon/trading-card-game"
-              >
-                TCG
-              </Link>
-            </li>
-          </ul>
+            <span className="self-center text-xl font-semibold whitespace-nowrap text-gray-9">
+              Benjoemon
+            </span>
+          </a>
+          <button
+            data-collapse-toggle="navbar-default"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-default"
+            aria-expanded="false"
+            onClick={toggleMenu}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <div
+            className={`${
+              menuOpen ? "mb-[-1.5rem] pl-4 pr-4 pb-4 pt-2" : "hidden"
+            } w-full md:block md:w-auto`}
+            id="navbar-default"
+          >
+            <ul className="font-bold flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  className="block py-2 px-3 text-gray-9 rounded m-0"
+                  aria-current="page"
+                  to="/pokemons"
+                >
+                  Pokémons
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className="block py-2 px-3 text-gray-9 rounded m-0"
+                  to="/pokemon/trading-card-game"
+                >
+                  TCG
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
